a7cbb1fabb45a1d18a69d7f73ba65105380f34a9
Anand Angalig <anandangalig@gmail.com>
2019-06-16 18:36:56 -0400

    add Prisma server and pre-made backend logic

--- src/index.js
+++ src/index.js
@@ -2,5 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+// httpLink connects ApolloClient instance with the GraphQL API, GraphQL server will be running on http://localhost:4000
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000',
+});
+// instantiate ApolloClient
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.querySelector('#root'),
+);
