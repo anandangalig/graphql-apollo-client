a081c4cd34626e4e6946b472f64a7c3fcfb1fbd7
Anand Angalig <anandangalig@gmail.com>
2019-06-24 17:36:11 -0400

    installed router; TIL withRouter external components gain access to the router history

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { ApolloProvider } from 'react-apollo';
 import { ApolloClient } from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { BrowserRouter } from 'react-router-dom';
 
 // httpLink connects ApolloClient instance with the GraphQL API, GraphQL server will be running on http://localhost:4000
 const httpLink = createHttpLink({
@@ -18,8 +19,10 @@ const client = new ApolloClient({
 });
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>,
   document.querySelector('#root'),
 );
