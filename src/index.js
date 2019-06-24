b8600feb4574ad0bb1bae5e2e3a9e39ceb00726c
Anand Angalig <anandangalig@gmail.com>
2019-06-24 18:31:59 -0400

    implemented authentication using localStorage + Apollo Link middleware

--- src/index.js
+++ src/index.js
@@ -7,14 +7,27 @@ import { ApolloClient } from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { BrowserRouter } from 'react-router-dom';
+import { setContext } from 'apollo-link-context';
+import { AUTH_TOKEN } from './constants';
 
 // httpLink connects ApolloClient instance with the GraphQL API, GraphQL server will be running on http://localhost:4000
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000',
 });
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN);
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
 // instantiate ApolloClient
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
